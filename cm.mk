c7c36500e3ca1fb502d3b44e365e79ad9aefac2a
john craig <johnccraig80@gmail.com>
2012-12-22 17:55:22 -0500

    Update fingerprint for 4.1.1(JRO03L)
    Change-Id: Ied72f78c2122f7cf02c6c6c3478cd54e7e09e676

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2tmo TARGET_DEVICE=d2tmo BUILD_FINGERPRINT="d2tmo-user 4.2.1 JOP4OD T999UVDLJA release-keys" PRIVATE_BUILD_DESC="samsung/d2tmo/d2tmo:4.2.1/JOP4OD/T999UVDLJA:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2tmo TARGET_DEVICE=d2tmo BUILD_FINGERPRINT="d2tmo-user 4.1.1 JRO03L T999UVDLJA release-keys" PRIVATE_BUILD_DESC="samsung/d2tmo/d2tmo:4.1.1/JRO03L/T999UVDLJA:user/release-keys"
 
 PRODUCT_NAME := cm_d2tmo
 PRODUCT_DEVICE := d2tmo
