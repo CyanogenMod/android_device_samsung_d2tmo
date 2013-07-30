9acfad56be07280ebdabbfac4430dc2f60649ac8
Dan Pasanen <dan.pasanen@gmail.com>
2013-07-30 13:19:35 -0500

    update fingerprint
    Change-Id: Ic66d3d0428c4635c76489228afcdb8d4999abddc

--- cm.mk
+++ cm.mk
@@ -9,7 +9,11 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2tmo TARGET_DEVICE=d2tmo BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.1.1/JRO03L/T999UVDLJA:user/release-keys" PRIVATE_BUILD_DESC="d2tmo-user 4.1.1 JRO03L T999UVDLJA release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2uc \
+    TARGET_DEVICE=d2tmo \
+    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.2/JZO54K/T999UVDMD5:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2uc-user 4.1.2 JZO54K T999UVDMD5 release-keys"
 
 PRODUCT_NAME := cm_d2tmo
 PRODUCT_DEVICE := d2tmo
