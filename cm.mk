81fde7617548cd547482852d6128d91c53059865
f3n1xu5 <fenixus@gmail.com>
2015-01-12 21:30:55 -0500

    Repairing a broken d2tmo baseband issue in CM12
    All files modified in same way as reflected in d2att commit 63bd4529dee7c300922b131599793af1dea27d8f
    Updated T999 baseband fingerprint to latest available in file cm.mk
    
    Amend: Updated fingerprint with latest available for d2tmo from a vendor blob.
    
    Change-Id: I3f37b485c4fe18e81d1d8fb5a06b7ab9f5272cf2

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2tmo \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.2/JZO54K/T999UVDMD5:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user 4.1.2 JZO54K T999UVDMD5 release-keys"
+    BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.3/JSS15J/T999UVUEMJC:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2tmo-user 4.3 JSS15J T999UVUEMJC release-keys"
 
 PRODUCT_NAME := cm_d2tmo
 PRODUCT_DEVICE := d2tmo
