b7e2b5612849895cabe09b88dab2e1f0606b15d9
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 03:36:00 -0600

    d2tmo: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Ic0aae99ada1ddc64eef402882a8b7ddd9e1b0d82

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/d2tmo/full_d2tmo.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
