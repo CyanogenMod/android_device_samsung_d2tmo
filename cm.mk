bf112aa8e63d030bc24795346da04c3af4f84210
Paul Keith <javelinanddart@gmail.com>
2016-12-07 13:40:57 -0500

    d2tmo: Remove duplicate inclusion of common_full_phone.mk
    * this is also included in d2-common
    
    Change-Id: Iae2593df0906e5aaa06b6a8b2170d9610b51e0ea

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/d2tmo/full_d2tmo.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2tmo \
